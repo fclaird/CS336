69c3db190ec6afd5d1c24c41e02ad403d70413b7
christopher laird <fclaird@yahoo.com>
2017-08-09 10:14:05 +0900

    _

---
+++ Lists/__init__.py
@@ -0,0 +1,17 @@
+options = {
+	'Workshop1': "Workshop Alpha",
+	'Workshop2': "Workshop Bravo",
+	'Workshop3': "Workshop Charlie",
+	'Workshop4': "Workshop Delta",
+	'Workshop5': "Workshop Echo",
+	'Workshop6': "Workshop Foxtrot",
+	'Workshop7': "Workshop Golf",
+	'Workshop8': "Workshop Helo",
+	'Workshop9': "Workshop India",
+	'mealpack': "Full Meal Package",
+	'dinnerday2': "Only Dinner on Day 2 (Awards Banquet)",
+	'allconference': "All Registrants of Conference"
+}
+
+
+
