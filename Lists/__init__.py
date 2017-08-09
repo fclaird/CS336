b6b74eacefd9623fcb0ec214010dd2fbf7469a8e
christopher laird <fclaird@yahoo.com>
2017-08-09 13:20:25 +0900

    -

--- Lists/__init__.py
+++ Lists/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 options = {
 	'Workshop1': "Workshop Alpha",
 	'Workshop2': "Workshop Bravo",
@@ -13,5 +15,57 @@ options = {
 	'allconference': "All Registrants of Conference"
 }
 
+class Registrant(dict):
+	_keys = [
+		"billing_firstname",
+		"address2",
+		"city",
+		"address1",
+		"lastname",
+		"title",
+		"email",
+		"card_type",
+		"website",
+		"firstname",
+		"state",
+		"card_csv",
+		"telephone",
+		"session3",
+		"exp_month",
+		"company",
+		"billing_lastname",
+		"meals",
+		"exp_year",
+		"card_number",
+		"position",
+		"session2",
+		"session1",
+		"zipcode",
+		"date_of_registration"]
+
+	def __init__(self, this_regestrant):
+		for key in self._keys:
+			self[key] = this_regestrant[key]
 
 
+
+def get_JSON_data():
+	data_path = 'registrant_data.json'
+	registrant_list = []
+
+	try:
+		with open(data_path, 'r') as my_file:
+			data_macro = json.load(my_file)
+
+			for i in range(len(data_macro['registrants'])):
+				info = data_macro['registrants'][i]
+				registrant_list.append(Registrant(info))
+
+			print(registrant_list[1]["city"])
+		my_file.close()
+
+	except IOError as e:
+		print("Unable to open file " + data_path)
+		quit(1)
+
+get_JSON_data()
