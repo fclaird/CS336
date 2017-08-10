5d89ae6a533045b4217d00e7463bf471aaf037f0
christopher laird <fclaird@yahoo.com>
2017-08-10 10:27:05 +0900

    -

--- Lists/__init__.py
+++ Lists/__init__.py
@@ -43,29 +43,53 @@ class Registrant(dict):
 		"zipcode",
 		"date_of_registration"]
 
+
+
 	def __init__(self, this_regestrant):
 		for key in self._keys:
 			self[key] = this_regestrant[key]
 
 
 
-def get_JSON_data():
+def get_json_data():
 	data_path = 'registrant_data.json'
 	registrant_list = []
-
 	try:
 		with open(data_path, 'r') as my_file:
 			data_macro = json.load(my_file)
-
 			for i in range(len(data_macro['registrants'])):
-				info = data_macro['registrants'][i]
-				registrant_list.append(Registrant(info))
-
-			print(registrant_list[1]["city"])
+				a_registrant = data_macro['registrants'][i]
+				registrant_list.append(Registrant(a_registrant))
 		my_file.close()
-
 	except IOError as e:
 		print("Unable to open file " + data_path)
 		quit(1)
+	sorted_list = sorted(registrant_list, key=lambda x: x['lastname'])
+	return sorted_list
 
-get_JSON_data()
+
+def generate_list(sorted_master_list, discriminator):
+	return [x for x in sorted_master_list if x['session1'] == discriminator or
+		x['session2'] == discriminator or x['session3'] == discriminator or
+		x['meals'] == discriminator]
+
+def generate_HTML(sorted_sublist, discriminator, actual_name):
+	with open(discriminator + '.html', 'w+') as html:
+		html.write("<link href='" + sorted_sublist + ".css' rel='stylesheet' type='text/css'/>")
+		html.write("\n<title> a sorted sub-list</title>")
+		html.write("\n	<div=sublist>")
+		html.write("\n	<h3>Here is a list of all the attendees who selected: " + actual_name + "</h3>")
+		html.write("\n<br>")
+		for obj in sorted_sublist:
+			html.write("<span>" + obj['lastname'] + ", " + obj['firstname'] + "</span><br>")
+		html.write("\n<br>")
+		html.write("\n<br>")
+		html.write("<span>The number of registrants that selected this option is: " + len(obj) + "</span><br>")
+		html.write("\n	</div>")
+		html.close()
+
+
+sorted_master_list = get_json_data()
+sorted_sublist = generate_list(sorted_master_list, "mealpack")
+
+
