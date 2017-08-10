2ae9a0af72a37701942914f349b24a19b2ae6317
christopher laird <fclaird@yahoo.com>
2017-08-11 03:33:33 +0900

    -

--- Lists/__init__.py
+++ Lists/__init__.py
@@ -1,15 +1,16 @@
 import json
+import os
 
 options = {
-	'Workshop1': "Workshop Alpha",
-	'Workshop2': "Workshop Bravo",
-	'Workshop3': "Workshop Charlie",
-	'Workshop4': "Workshop Delta",
-	'Workshop5': "Workshop Echo",
-	'Workshop6': "Workshop Foxtrot",
-	'Workshop7': "Workshop Golf",
-	'Workshop8': "Workshop Helo",
-	'Workshop9': "Workshop India",
+	'Workshop A': "Workshop Alpha",
+	'Workshop B': "Workshop Bravo",
+	'Workshop C': "Workshop Charlie",
+	'Workshop D': "Workshop Delta",
+	'Workshop E': "Workshop Echo",
+	'Workshop F': "Workshop Foxtrot",
+	'Workshop G': "Workshop Golf",
+	'Workshop H': "Workshop Helo",
+	'Workshop I': "Workshop India",
 	'mealpack': "Full Meal Package",
 	'dinnerday2': "Only Dinner on Day 2 (Awards Banquet)",
 	'allconference': "All Registrants of Conference"
@@ -49,8 +50,6 @@ class Registrant(dict):
 		for key in self._keys:
 			self[key] = this_regestrant[key]
 
-
-
 def get_json_data():
 	data_path = 'registrant_data.json'
 	registrant_list = []
@@ -69,27 +68,33 @@ def get_json_data():
 
 
 def generate_list(sorted_master_list, discriminator):
-	return [x for x in sorted_master_list if x['session1'] == discriminator or
+	if discriminator == 'allconference': return sorted_master_list
+	else: return [x for x in sorted_master_list if (x['session1'] == discriminator or
 		x['session2'] == discriminator or x['session3'] == discriminator or
-		x['meals'] == discriminator]
+		x['meals'] == discriminator)]
 
-def generate_HTML(sorted_sublist, discriminator, actual_name):
-	with open(discriminator + '.html', 'w+') as html:
-		html.write("<link href='" + sorted_sublist + ".css' rel='stylesheet' type='text/css'/>")
+def generate_html(sorted_sublist, discriminator, actual_name):
+	here = os.path.dirname(os.path.realpath(__file__))
+	file_name = discriminator + '.html'
+	file_path = os.path.join(here, 'participantlists', file_name)
+	with open(file_path, 'w+') as html:
+		html.write("<link href='sorted_sublist.css' rel='stylesheet' type='text/css'/>")
 		html.write("\n<title> a sorted sub-list</title>")
-		html.write("\n	<div=sublist>")
-		html.write("\n	<h3>Here is a list of all the attendees who selected: " + actual_name + "</h3>")
-		html.write("\n<br>")
-		for obj in sorted_sublist:
-			html.write("<span>" + obj['lastname'] + ", " + obj['firstname'] + "</span><br>")
-		html.write("\n<br>")
-		html.write("\n<br>")
-		html.write("<span>The number of registrants that selected this option is: " + len(obj) + "</span><br>")
-		html.write("\n	</div>")
-		html.close()
-
+		html.write("\n<div class=sublist>")
+		html.write("\n	<h3>Here is a list of all the attendees who selected: " + actual_name + "</h3><br>")
+		if sorted_sublist != []:
+			for reg in sorted_sublist:
+				html.write("\n	<span>" + reg['lastname'] + ", " + reg['firstname'] + "</span><br>")
+			html.write("\n	<br><br><span>The number of registrants that selected this option is: " + str(len(sorted_sublist)) + "</span>")
+		else: html.write("\n	<br><br><span>The number of registrants that selected this option is: 0 </span>")
+	html.close()
 
 sorted_master_list = get_json_data()
-sorted_sublist = generate_list(sorted_master_list, "mealpack")
 
 
+for discriminant in options.keys():
+	sorted_sublist = generate_list(sorted_master_list, discriminant)
+
+
+
+	generate_html(sorted_sublist, discriminant, options[discriminant])
